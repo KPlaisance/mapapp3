9c3ed2d4b4e614fdcbadbe5c246288fe4798ab35
Michael Plaisance <MPlaisance@paylocity.com>
2018-11-28 23:51:24 -0700

    Duplicate files removed

--- src/index.js
+++ src/index.js
@@ -6,16 +6,6 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-//  Registering service worker
-
-// if ('serviceWorker' in navigator){
-//     navigator.serviceWorker
-//     .register('/src/serviceWorker.js')
-//     .catch(function(err) {
-//       console.error(err);
-//     });
-//   }
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
